56c0f6134bba7f95ede697ad52837b090d1f3162
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-02 14:36:31 +1100

    Update return type

--- src/execute.rs
+++ src/execute.rs
@@ -1,10 +1,11 @@
 use crate::parser;
 
 
-pub fn execute(tokens: &Vec<parser::ParsedTokens>) {
+pub fn execute(tokens: &Vec<parser::ParsedTokens>) -> Result<(), String> {
 
     for (index, token) in tokens.iter().enumerate() {
         
     }
 
+    Ok(())
 }
